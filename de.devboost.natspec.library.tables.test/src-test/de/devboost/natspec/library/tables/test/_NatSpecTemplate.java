3cdc7c9435ffce9f90d15196e7af4d36e7c49c95
Christian Wende <christian.wende@devboost.de>
2013-07-08 13:49:20 +0200

    added example project to document usage of natspec tables

---
+++ de.devboost.natspec.library.tables.test/src-test/de/devboost/natspec/library/tables/test/_NatSpecTemplate.java
@@ -0,0 +1,17 @@
+package de.devboost.natspec.library.tables.test;
+
+import org.junit.Test;
+
+import de.devboost.natspec.library.tables.support.TableSyntaxSupport;
+
+public class _NatSpecTemplate {
+
+	protected TableSyntaxSupport tableSyntaxSupport = new TableSyntaxSupport();
+	protected CalculatorTestSupport calculatorTestSupport = new CalculatorTestSupport();
+	
+	@Test
+	public void executeScript() throws Exception {
+		/* @MethodBody */
+	}
+
+}
